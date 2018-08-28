f5696c62b8dd4050560dbb9ada1670fe6a216485
sanatiel90 <sanatielbarros@gmail.com>
2018-08-28 20:23:27 -0300

    atualizando seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(ExpensesTableSeeder::class);
+        //$this->call(ExpensesTableSeeder::class);
     }
 }
