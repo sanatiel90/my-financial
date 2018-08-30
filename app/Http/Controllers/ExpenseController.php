daed104611bc4b3f8e155386e0a62b4c2ec71579
sanatiel90 <sanatielbarros@gmail.com>
2018-08-29 21:33:40 -0300

    testes

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -119,6 +119,7 @@ class ExpenseController extends Controller
     public function expensesMonthlyDetail(Request $request)
     {
         echo "algo";
+        var_dump($expensesMonth['all']);
         //$expensesMonth['all'] = Expense::expensesDetail($request->month, 2); 
         //$expensesMonth['categ'] = Expense::expensesDetail($request->month, 1);
         //return response()->json($expensesMonth);
