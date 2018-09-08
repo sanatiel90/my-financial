3b3378da9a42e71daf524a777dfa18ca7e84a512
sanatiel90 <sanatielbarros@gmail.com>
2018-09-08 17:34:46 -0300

    test grafic

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -105,7 +105,7 @@ class ExpenseController extends Controller
     //monta o json para alimentar o gráfico de despesas por categoria
     public function getJsonChart(){
 
-        /*$jsonChart = '{
+        $jsonChart = '{
                     "cols": [
                         {"id":"","label":"Category","pattern":"","type":"string"},
                         {"id":"","label":"Total","pattern":"","type":"number"}
@@ -116,22 +116,9 @@ class ExpenseController extends Controller
             $jsonChart .= '{"c":[{"v":"'.$v->name_categ.'/'.$v->name_sub_categ.'","f":null},{"v":'.$v->sumCateg.',"f":null}]},';
         }
         $jsonChart .= ' ] }';
-          */
-        $jsonChart = '{
-  "cols": [
-        {"id":"","label":"Topping","pattern":"","type":"string"},
-        {"id":"","label":"Slices","pattern":"","type":"number"}
-      ],
-  "rows": [
-        {"c":[{"v":"Mushrooms","f":null},{"v":3,"f":null}]},
-        {"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
-        {"c":[{"v":"Olives","f":null},{"v":1,"f":null}]},
-        {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
-        {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
-      ]
-}'; 
-
-        return $jsonChart;
+        
+        echo $jsonChart;          
+        //return $jsonChart;
     }
 
 
